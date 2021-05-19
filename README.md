b6c6d0518e5d58d362d6c99a48016b2d320f4efd
ivette.horna <ivette.vhorna@gmail.com>
2021-05-18 21:38:10 -0500

    delete video

--- README.md
+++ README.md
@@ -1,15 +1,5 @@
 # Demo-Brein
 
-Configurations in Eclipse:
-___________________________
-
-The tools used to automate eclipse
-The configurations that must be made before executing the project are the following:
-
-Project->properties->Libraries->add Jars
-|
--> /src/test/resources/ejecutables/ATUTestRecorder_2.1.jar
--> /src/test/resources/ejecutables/ATUReporter_testNG_2.1.jar
 
 Configurations in Project:
 ___________________________                                   
--- src/test/java/com/brein/testng/LoginTest.java
+++ src/test/java/com/brein/testng/LoginTest.java
@@ -4,19 +4,12 @@ import org.testng.annotations.AfterClass;
 import org.testng.annotations.Test;
 import org.testng.annotations.BeforeClass;
 import org.openqa.selenium.WebDriver;
-import atu.testrecorder.ATUTestRecorder;
-import org.testng.annotations.AfterTest;
-import org.testng.annotations.BeforeSuite;
-import org.testng.annotations.BeforeTest;
-import java.io.File;
-import java.text.DateFormat;
-import java.text.SimpleDateFormat;
-import java.util.Date;
+ 
 
 public class LoginTest {
 
 	  private WebDriver driver;
-	  private ATUTestRecorder recorder;
+	 
 	  LoginPage loginPage;
 	
 	  
@@ -29,37 +22,14 @@ public class LoginTest {
 	  public void beforeClass() {
 		  loginPage = new LoginPage(driver);
 		  driver = loginPage.chromeDriverConnection("W");
-	  loginPage.visit("https://frida-clientes-development.web.app/");
+	      loginPage.visit("https://frida-clientes-development.web.app/");
 	  }
 	
 	  @AfterClass
-	public void afterClass() {
+	  public void afterClass() {
 		  driver.close();
 	  }
 	  
-		@BeforeTest
-	public void beforeTest() throws Exception{
-			 DateFormat dateFormat = new SimpleDateFormat("yy-MM-dd HH-mm-ss");
-	  Date date = new Date();		  
-	  recorder = new ATUTestRecorder(System.getProperty("user.dir")+"/videos",dateFormat.format(date),false);		 
-		  recorder.start();	
-	}
-	
-	@AfterTest
-	public void afterTest() throws Exception{
-		recorder.stop();
-	}
-	
-	@BeforeSuite
-	public synchronized void beforeSuite() throws Exception{
-		deletevideos(System.getProperty("user.dir")+"/videos");
-	}
-	public static void deletevideos(String path) {
-		File directory = new File(path);
-		File[] files = directory.listFiles();
-		for (File file: files) {
-			file.delete();
-		}
-	}
+
 
 }
